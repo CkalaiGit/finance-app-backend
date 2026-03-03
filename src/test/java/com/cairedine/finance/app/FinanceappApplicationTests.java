69097962a90fc19cbf111248e702bc8c2e443098
CkalaiGit <cairedine@gmail.com>
2026-03-03 01:30:56 +0100

    feat: configure server port and update issuer URI in application.yaml; enable test profile in tests

--- src/test/java/com/cairedine/finance/app/FinanceappApplicationTests.java
+++ src/test/java/com/cairedine/finance/app/FinanceappApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinanceappApplicationTests {
 
 	@Test
